293ccb86a47f213404eed93fc0dcbda7012473c3
fetcheping <cabreltchomte97@gmail.com>
2022-05-27 08:47:50 +0100

    evo_purchase_order_automation

--- evo_purchase_order_automation/__manifest__.py
+++ evo_purchase_order_automation/__manifest__.py
@@ -3,13 +3,16 @@
     "version": "14.0",
     "author": "Evozard",
     "category": "Purchase",
-    "summary": """Enable auto purchase workflow with purchase order confirmation. Include operations like Auto Create Invoice, Auto Validate Invoice and Auto Transfer Delivery Order.""",
+    "summary": """Enable auto purchase workflow with purchase order 
+                  confirmation. Include operations like Auto Create Invoice, 
+                  Auto Validate Invoice and Auto Transfer Delivery Order.""",
     "description": """
-        You can directly create invoice and set done to delivery order by single click
+        You can directly create invoice and set done to delivery order by single
+         click
     """,
     "website": "www.evozard.com",
     "depends": ["purchase", "stock", "stock_operating_unit"],
-    "data": ["views/stock_warehouse.xml",],
+    "data": ["views/stock_warehouse.xml"],
     "installable": True,
     "application": True,
     "auto_install": False,
